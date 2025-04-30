69202ffd6037b430f77619897873be15ebe478ed
Clayton Kruse <claytonkruse08@gmail.com>
2025-04-30 17:05:52 -0500

    Dog

--- src/org/mizzoucs3330team/assignment_05/App.java
+++ src/org/mizzoucs3330team/assignment_05/App.java
@@ -1,8 +1,11 @@
 package org.mizzoucs3330team.assignment_05;
+
+import org.mizzoucs3330team.assignment_05.model.pets.Dog;
+
 //import org.mizzoucs3330team.assignment_05.model.pets.Pet;
 
-public class App{
-    public static void main(String[] args) {
-        //Pet myPet = new Pet();
-    }
+public class App {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+	}
 }
--- src/org/mizzoucs3330team/assignment_05/model/pets/Dog.java
+++ src/org/mizzoucs3330team/assignment_05/model/pets/Dog.java
@@ -1,5 +1,16 @@
 package org.mizzoucs3330team.assignment_05.model.pets;
 
 public class Dog extends Pet {
-    //TODO: dog pet properties
+
+	/**
+	 * Create a Dog.
+	 * 
+	 * @param name    The dog's name.
+	 * @param age     The dog's age in years.
+	 * @param adopted Set to true if the dog has an owner; false otherwise.
+	 */
+	public Dog(String name, int age, boolean adopted) {
+		super(name, age, adopted);
+	}
+
 }
