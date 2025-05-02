c22cd11ad5bd7d3715deebc1bf1072fe5a8c0596
Clayton Kruse <claytonkruse08@gmail.com>
2025-05-02 18:53:30 -0500

    setPets

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -10,10 +10,13 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import main.java.model.pets.Pet;
+
 public class View extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JTable table;
+	DefaultTableModel tableModel;
 
 	/**
 	 * Launch the application.
@@ -51,7 +54,7 @@ public class View extends JFrame {
 
 		String[] colNames = { "Pet ID", "Name", "Type", "Species", "Age", "Adopted?", "Actions" };
 		Object[][] data = {};
-		DefaultTableModel tableModel = new DefaultTableModel(data, colNames);
+		tableModel = new DefaultTableModel(data, colNames);
 		table = new JTable(tableModel);
 		scrollPane.setViewportView(table);
 
@@ -83,6 +86,30 @@ public class View extends JFrame {
 		table.getColumnModel().getColumn(5).setMaxWidth(60);
 
 		tableModel.addRow(new Object[] { 1, "Harold", "Pig", "Pink", 2, "No", "TODO: Delete Btn, etc." });
+
+	}
+
+	/**
+	 * Set the pets that are displayed inside the table.
+	 * 
+	 * @param pets The pets to display.
+	 */
+	public void setPets(Pet[] pets) {
+		// Remove all existing rows.
+		while (tableModel.getRowCount() > 0) {
+			tableModel.removeRow(0);
+		}
+
+		for (Pet pet : pets) {
+			String typeString = "Unknown";
+			String speciesString = "Unknown";
+
+			// TODO: Implement an if/else for type checking. This could be implemented as a
+			// static method of Pet.
+
+			tableModel.addRow(new Object[] { pet.getId(), pet.getName(), typeString, speciesString, pet.getAge(),
+					pet.getAdopted() ? "Yes" : "No", "TODO: Delete Btn, other actions etc." });
+		}
 
 	}
 }
